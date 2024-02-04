6c26182eaf0ac732607853ba3139f437df65a188
mpavle1 <102835309+mpavle1@users.noreply.github.com>
2024-02-04 21:28:23 +0100

    init

---
+++ day_03_gear_ratios/src/main.rs
@@ -0,0 +1,18 @@
+const input: &str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+fn main() {
+    let dot = ".";
+
+    let rows: Vec<&str> = input.split("\n").collect();
+
+    println!("Hello, world!");
+}
